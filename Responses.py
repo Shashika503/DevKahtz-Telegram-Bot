80a4ce6df0bf8b496a1f5c87b9a28d3eec5f4132
Shashika503 <kahatapitiyagsj@gmail.com>
2022-01-05 21:31:02 +0530

    Create a main

---
+++ Responses.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+def sample_responses(input_text):
+    user_message = str(input_text).lower()
+
+
+    if user_message in ("hello", "hi", "what's Up",):
+        return "Hey! How's it going?"
+
+    if user_message in ("who are you","who are you?"):
+        return "I am TechieKhtz!"
+
+    if user_message in ("Could plz tell the time now?" , "time"):
+        now = datetime.now()
+        date_time = now.strftime("%d%m%y, %H:%M:%S")
+
+    return str(date_time)
+
+    return "I don't understand you."
+
+
